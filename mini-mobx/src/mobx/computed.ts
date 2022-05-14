7f47aed61b8c1d06463ee2be123957ccdd47db6e
shuangxu <shuangxu@dtstack.com>
2022-05-14 16:19:46 +0800

    feat: user proxy for computed

--- mini-mobx/src/mobx/computed.ts
+++ mini-mobx/src/mobx/computed.ts
@@ -3,13 +3,18 @@ import dependenceManager from "./dependenceManager";
 
 export function computed(target: any, name: any, descriptor: any) {
     const getter = descriptor.get; // get 函数
-    const _watcher = new ComputeWatcher(target, getter);
+    const _watcher = new ComputeWatcher(target);
+    const proxyValue = new Proxy(target, {
+        get: (target) => {
+            return getter.call(target);
+        }
+    })
     return {
         enumerable: true,
         configurable: true,
         get: function () {
-            _watcher.target = this
-            return _watcher.get();
+            _watcher.get()
+            return proxyValue[name]
         }
     };
 }
@@ -17,14 +22,11 @@ export function computed(target: any, name: any, descriptor: any) {
 class ComputeWatcher {
     id: string;
     target: any;
-    getter: any;
     // 标识是否绑定过computed的依赖，只需要绑定一次
-    hasBindAutoReCompute: any;
-    value: any;
-    constructor(target: any, getter: any) {
+    hasBindAutoReCompute: boolean | undefined;
+    constructor(target: any) {
         this.id = `computed_${shortid()}`
         this.target = target
-        this.getter = getter
     }
     // 绑定recompute 和 内部涉及到的观察值的关系
     _bindAutoReCompute() {
@@ -37,13 +39,11 @@ class ComputeWatcher {
     }
     // 依赖属性变化时调用的函数
     _reComputed() {
-        this.value = this.getter.call(this.target);
         dependenceManager.trigger(this.id);
     }
     // 提供给外部调用时收集依赖使用
     get() {
         this._bindAutoReCompute()
         dependenceManager.collect(this.id);
-        return this.value
     }
 }
