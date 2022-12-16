6097a9bbd78482ce4d18898ed451bba11d213994
LuckyFBB <976060700@qq.com>
2022-12-16 10:15:28 +0800

    update file

---
+++ node-file/index.js
@@ -0,0 +1,65 @@
+// fs API 文件拷贝
+// const fs = require("fs");
+// const path = require("path");
+
+// const sourceFile = path.resolve(__dirname, "../doc/Mobx原理及丐版实现.md");
+// const targetFile = path.resolve(__dirname, "target.txt");
+
+// fs.readFile(sourceFile, (err, data) => {
+//     if (err) throw err;
+//     const dataStr = data.toString();
+//     fs.writeFile(targetFile, dataStr, (err) => {
+//         if (err) throw err;
+//         console.log("copy success~");
+//         process.exit(1);
+//     });
+// });
+
+// fs buffer 文件拷贝
+// const fs = require("fs");
+// const path = require("path");
+
+// const copyFile = (source, target, size, callback) => {
+//     const sourceFile = path.resolve(__dirname, source);
+//     const targetFile = path.resolve(__dirname, target);
+
+//     const buf = Buffer.alloc(size);
+//     let hasRead = 0; // 下次读取文件的位置
+//     let hasWrite = 0; // 下次写入文件的位置
+//     fs.open(sourceFile, "r", (err, sourceFd) => {
+//         if (err) callback(err);
+//         fs.open(targetFile, "w", (err, targetFd) => {
+//             if (err) throw callback(err);
+//             function next() {
+//                 fs.read(sourceFd, buf, 0, size, hasRead, (err, bytesRead) => {
+//                     if (err) callback(err);
+//                     hasRead += bytesRead;
+//                     if (bytesRead) {
+//                         fs.write(targetFd, buf, 0, size, hasWrite, (err, bytesWrite) => {
+//                             if (err) callback(err);
+//                             hasWrite += bytesWrite;
+//                             next();
+//                         });
+//                         return;
+//                     }
+//                     fs.close(sourceFd, () => { console.log("关闭源文件"); });
+//                     fs.close(targetFd, () => { console.log("关闭目标文件"); });
+//                 });
+//             }
+//             next();
+//         });
+//     });
+// };
+
+// copyFile("../doc/Mobx原理及丐版实现.md", "target.txt", 8, (err) => {
+//     throw err;
+// });
+
+// Stream 使用
+const fs = require("fs");
+const path = require("path");
+const readStream = fs.createReadStream(
+    path.resolve(__dirname, "../doc/Mobx原理及丐版实现.md")
+);
+const writeStream = fs.createWriteStream(path.resolve("target.txt"));
+readStream.pipe(writeStream);
