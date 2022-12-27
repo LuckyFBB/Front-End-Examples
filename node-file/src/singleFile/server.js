ccd1af816292f9a6099bd0a5a3244231895a91cf
LuckyFBB <976060700@qq.com>
2022-12-27 19:38:03 +0800

    feat: add file code

---
+++ node-file/src/singleFile/server.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const Koa = require("koa");
+const serve = require("koa-static");
+const cors = require("@koa/cors");
+const multer = require("@koa/multer");
+const Router = require("@koa/router");
+
+const app = new Koa();
+const router = new Router();
+const PORT = 3000;
+// 上传后资源的URL地址
+const RESOURCE_URL = `http://localhost:${PORT}`;
+// 存储上传文件的目录
+const UPLOAD_DIR = path.join(__dirname, "/public/upload");
+
+const storage = multer.diskStorage({
+    destination: async function (req, file, cb) {
+        // 设置文件的存储目录
+        cb(null, UPLOAD_DIR);
+    },
+    filename: function (req, file, cb) {
+        // 设置文件名
+        cb(null, `${file.originalname}`);
+    },
+});
+
+const multerUpload = multer({ storage });
+
+router.get("/", async (ctx) => {
+    ctx.body = "yohe, welcome";
+});
+
+router.post("/uploadSingle", async (ctx) => {
+    try {
+        await next();
+        ctx.body = {
+            code: 1,
+            msg: "文件上传成功",
+            url: `${RESOURCE_URL}/${ctx.file.originalname}`,
+        };
+    } catch (error) {
+        ctx.body = {
+            code: 0,
+            msg: "文件上传失败",
+        };
+    }
+    multerUpload.single("file");
+});UPLOAD_DIR
+
+// 注册中间件
+app.use(cors());
+app.use(serve());
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(PORT, () => {
+    console.log("server start on ", PORT);
+});
